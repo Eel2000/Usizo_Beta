4b4ba4637a770cb4b97a18ed75d2f48444f9295a
Eel2000 <eliezerbwana@gmaill.com>
2020-11-10 14:26:15 +0100

    🚧Desplay Activity Details On Details Page

--- Usizo_Beta/Usizo_Beta/ViewModels/DetailPageViewModel.cs
+++ Usizo_Beta/Usizo_Beta/ViewModels/DetailPageViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Usizo_Beta.Models;
 using Usizo_Beta.Services;
 using Xamarin.Forms;
@@ -14,6 +16,9 @@ namespace Usizo_Beta.ViewModels
 
         public string Title { get; set; }
         public string ActiviteID { get; set; }
+        public string NombreEnfant { get; set; }
+
+        public ICommand AddChildrenCommand { get; set; }
 
         public DetailPageViewModel(INavigation navigation)
         {
@@ -22,7 +27,6 @@ namespace Usizo_Beta.ViewModels
 
         public DetailPageViewModel(Activite activite)
         {
-            //GetDetails(activite.UID);
             Nom = activite.Nom;
             UID = activite.UID;
             Lieu = activite.Lieu;
@@ -30,21 +34,29 @@ namespace Usizo_Beta.ViewModels
             Date = activite.Date;
             Enfants = activite.Enfants;
 
+            CountActivityChilds(Enfants);
+
+            AddChildrenCommand = new Command(async () => await AddChildren());
         }
 
-        //public async void GetDetails(string id)
-        //{
-        //    var result = await ActiviteServices.ActiviteService.ActiviteDetails(id);
-        //    Nom = result.Nom;
-        //    UID = result.UID;
+        public void CountActivityChilds(IEnumerable<Enfant> enfants)
+        {
 
-        //    Title = Nom;
-        //}
+            if (enfants == null)
+            {
+                NombreEnfant = "0";
+            }
+            else
+            {
+                var nombre = enfants.ToList();
+                NombreEnfant = nombre.Count.ToString();
+            }
+        }
 
-        //private async Task<Activite> GetActivite(string id)
-        //{
-        //    var result = await ActiviteServices.ActiviteService.ActiviteDetails(id);
-        //    return result;
-        //}
+        private async Task AddChildren()
+        {
+            await App.Current.MainPage.DisplayAlert("Information", "Vous avez ajouter un enfant a cet activité.", "ok");
+        }
+
     }
 }
